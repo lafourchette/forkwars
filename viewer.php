3f2d672fd641682f8f02138806fce318a59a9442
rodrigue67 <rodrigue67@users.noreply.github.com>
2014-07-30 15:26:37 +0200

    Update viewer.php
    Modification du Viewer.php pour intégrer la librairy pixi.js pour la gestion de la map en canvas ou webGL au lieu d'un quadrillage en DOM.
    
    Rodrigue DAL.

--- viewer.php
+++ viewer.php
@@ -38,18 +38,92 @@ $world = $worldFactory->make($mapString);
 .t.i{background-position: -828px -614px; }
 .t.x{background-position: -880px -614px; }
 </style>
+<!--
+On inclus la librairy pixi.js, possibilité de l'intégrer directement au projet avec un lien relatif'
+
+-->
+<script type="text/javascript" src="http://www.goodboydigital.com/pixijs/bunnymark/js/pixi.js"></script>
+
+<script type="text/javascript">
+
+    var heightWorld = <?php echo $world->height; ?>;
+    var widthWorld = <?php echo $world->width; ?>;
+    
+    //on stocke les textures des différents terrains pour ne pas les initialiser plus d'une fois (gain de ressources et de temps)
+    var texturesTerrains = new Array();
+    
+    // create an new instance of a pixi stage
+	var stage = new PIXI.Stage(0x97c56e, true);
+	// create a renderer instance
+	var renderer = PIXI.autoDetectRenderer(widthWorld*32*2, heightWorld*32*2, null);
+	
+	// add the renderer view element to the DOM
+	document.body.appendChild(renderer.view);
+	renderer.view.style.position = "absolute";
+	renderer.view.style.top = "0px";
+	renderer.view.style.left = "0px";
+	requestAnimFrame( animate );
+	
+	// create a texture from an image path
+    /*
+        On parcours la grille des cases pour récupérer le code et appliqué le masque correspondant dans le canvas.
+        On pourrais parcourir directement la grille basic.map mais il faudrait réencoder la logique en JS (1er ligne = nom, 2eme dimensions, ligne suivante etc...)
+
+    */
+    
+<?php for($y = 0; $y < $world->height; $y++) :?>
+
+<?php for($x = 0; $x < $world->width; $x++) :?>
+
+    createTerrain(<?php echo $world->getTerrain(new \Forkwars\Position($x, $y))->getCode(); ?>, <?php echo $x; ?>, <?php echo $y; ?>)
+
+<?php endfor; ?>
+
+<?php endfor; ?>
+
+
+	function createTerrain(code, x, y)
+	{
+		var iconeTerrain = code+".png";
+
+        if(!texturesTerrains[iconeTerrain])
+        {
+    		var texture = PIXI.Texture.fromImage(iconeTerrain);
+    		texturesTerrains[iconeTerrain] = texture;
+        }
+
+		var terrain = new PIXI.Sprite(texturesTerrains[iconeTerrain]);
+		
+		terrain.anchor.x = 0.5;
+		terrain.anchor.y = 0.5;
+		
+		//on prend la taille de l'image png sans agrandissement.
+		terrain.scale.x = terrain.scale.y = 2;
+		
+		// move the sprite to its designated position
+        //on calcule le x par rapport a la matrice de la grille en prenant en compte
+        //la taille en px de la zone ainsi que l'échelle car aucun terrain ne dois se chauvaucher.
+		terrain.position.x = x*32*2;
+		terrain.position.y = y*32*2;
+		
+		// add it to the stage
+		stage.addChild(terrain);
+	}
+	
+	function animate() {
+	
+	    requestAnimFrame( animate );
+	
+	    renderer.render(stage);
+	}
+
+
+
+</script>
+
 </head>
 <body>
 
-<?php for($y = 0; $y < $world->height; $y++) :?>
-    <div class="r">
-<?php for($x = 0; $x < $world->width; $x++) :?>
-
-    <div class="t <?php echo $world->getTerrain(new \Forkwars\Position($x, $y))->getCode(); ?>"></div>
-
-<?php endfor; ?>
-    </div>
-<?php endfor; ?>
 
 </body>
 </html>
